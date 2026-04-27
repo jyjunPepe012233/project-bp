6f9f8edc84b2487a043002f9c908d14c4da7281f
jyjunPepe012233 <jinyj0217@gmail.com>
2026-04-27 23:21:51 +0900

    init

---
+++ src/main/java/com/jyjun/projectbp/ProjectBpApplication.java
@@ -0,0 +1,13 @@
+package com.jyjun.projectbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jyjun/projectbp/ProjectBpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyjun.projectbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
