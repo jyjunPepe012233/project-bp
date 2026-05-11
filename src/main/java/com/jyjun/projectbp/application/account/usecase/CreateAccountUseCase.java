23782af1d9e465ed705c5fe5c3b10258d65156f4
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-11 20:34:14 +0900

    add note

--- src/main/java/com/jyjun/projectbp/application/account/usecase/CreateAccountUseCase.java
+++ src/main/java/com/jyjun/projectbp/application/account/usecase/CreateAccountUseCase.java
@@ -7,6 +7,10 @@ import com.jyjun.projectbp.domain.account.model.Account;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+// 이 API 안 쓸 것 같음.
+// 계정 생성은 개발자 생성하면서 루트 계정이 자동으로 생성되고, 이 루트 계정이 하위 계정을 만드는 방식으로 할 것 같음.
+// 즉 아무 개발자와 연결되지 않은, 쓸모없는 계정이 존재할 수도 있으므로 독립된 계정을 생성하는 API는 안 만들 듯
+
 @Service
 public class CreateAccountUseCase {
 
