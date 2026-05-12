ffae0ea2edef57a4bd965c558367adeea9ebe5cc
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-12 18:10:01 +0900

    make cors disabled

--- src/main/java/com/jyjun/projectbp/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/jyjun/projectbp/infrastructure/security/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.disable())
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
